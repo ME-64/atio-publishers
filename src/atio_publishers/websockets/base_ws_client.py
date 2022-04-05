ac11b1b0e2a5c272930ed78e3880a10b3dd7184a
ME-64 <miloelliott64@gmail.com>
2022-04-05 20:19:26 +0100

    added the worker ABC

--- src/atio_publishers/websockets/base_ws_client.py
+++ src/atio_publishers/websockets/base_ws_client.py
@@ -15,33 +15,34 @@ log: logging.Logger = logging.getLogger('atio')
 
 class BaseWSClient(ABC):
 
-    def __init__(self, ws_url: str, redis_url: str, redis_channel: str):
+    def __init__(self, ws_url: str, redis_url: str, redis_channel: str,#{{{
+            worker: Worker, publisher: Publisher):
         self.ws_url: str = ws_url
         self._started: asyncio.Event = asyncio.Event()
         self.pub_queue: aiop.Queue = aip.Queue()
         self.work_queue: aiop.Queue = aiop.Queue()
-        self.publisher: Publisher = Publisher(redis_url=redis_url,
+        self.publisher: Publisher = publisher(redis_url=redis_url,
                 redis_channel=redis_channel,
                 pub_queue=self.pub_queue)
-
-
-    @abstractmethod
-    async def subscribe(self):
-        pass
+        self.worker: Worker = worker(self.work_queue, self.pub_queue)# }}}
 
     @abstractmethod
-    async def unsubscribe(self):
-        pass
+    async def subscribe(self):# {{{
+        pass# }}}
 
     @abstractmethod
-    async def on_start(self):
-        pass
+    async def unsubscribe(self):# {{{
+        pass# }}}
 
     @abstractmethod
-    async def on_message(self, msg: str) -> None:
-        pass
+    async def on_start(self):# {{{
+        pass# }}}
 
-    async def start(self) -> None:
+    @abstractmethod
+    async def on_message(self, msg: str) -> None:# {{{
+        pass# }}}
+
+    async def start(self) -> None:# {{{
         self.session: aiohttp.ClientSession = aiohttp.ClientSession()
         self.ws: aiohttp.client_ws.ClientWebSocketResponse = await self.session.ws_connect(self.ws_url)
         self.publisher.start()
@@ -57,37 +58,57 @@ class BaseWSClient(ABC):
                 log.debug(f'msg received: {msg}')
         # when the websocket connection is closed, we disconnect
         # and let the container handle reconnecting
-        os.kill(os.getpid(), signal.SIGINT)
+        os.kill(os.getpid(), signal.SIGINT)# }}}
 
 
 class Publisher:
 
-    def __init__(self, redis_url: str, redis_channel: str, pub_queue: aiop.Queue):
+    def __init__(self, redis_url: str, redis_channel: str, pub_queue: aiop.Queue):# {{{
         self._started: asyncio.Event = asyncio.Event()
         self.redis_url: str = redis_url
         self.redis_channel: str = redis_channel
-        self.pub_queue: aiop.Queue = pub_queue
+        self.pub_queue: aiop.Queue = pub_queue# }}}
 
-
-    async def _start(self) -> None:
+    async def _start(self) -> None:# {{{
         self.redis: redis.asycnio.client.Redis = aioredis.from_url(self.redis_url, decode_responses=True)
         await self.redis.ping()
         self._started.set()
         while True:
             try:
                 to_pub: dict = await self.pub_queue.coro_get()
-                await self.redis.publish('mkt_data', ujson.dumps(to_pub))
+                await self.redis.publish(self.redis_channel, ujson.dumps(to_pub))
             except:
-                os.kill(os.getpid(), signal.SIGINT)
+                os.kill(os.getpid(), signal.SIGINT)# }}}
 
-    async def _run(self) -> None:
+    async def _run(self) -> None:# {{{
         loop: asyncio.unix_events._UnixSelectorEventLoop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.create_task(self._start())
+        loop.create_task(self._start())# }}}
 
-
-    async def start(self) -> None:
+    def start(self) -> None:# {{{
         self.proc: mp.Process = mp.Process(target=self._run)
-        self.proc.start()
+        self.proc.start()# }}}
 
 
+class Worker(ABC):
+
+    def __init__(self, work_queue: aiop.Queue, pub_queue: aiop.Queue):# {{{
+        self.work_queue: aiop.Queue = work_queue
+        self.pub_queue: aiop.Queue = pub_queue# }}}
+
+    @abstractmethod
+    def do_work(self) -> dict:# {{{
+        pass# }}}
+
+    def _run(self) -> None:# {{{
+        while True:
+            try:
+                work  = self.work_queue.get()
+                result: dict = self.do_work(work)
+                self.pub_queue.put(result)
+            except:
+                os.kill(os.getpid(), signal.SIGINT)# }}}
+
+    def start(self) -> None:# {{{
+        self.proc: mp.Process = mp.Process(target=self._run)
+        self.proc.start()# }}}
